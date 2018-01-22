dc7b84e7d485318e0576b27196a6a1e3f87afe37
janice <janicejiang.d@gmail.com>
2018-01-22 22:05:45 +0800

    Layout Links Test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,24 +6,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
   end
 
   test "should get home" do
-    get static_pages_home_url # 向 static_pages_home_url 发起 GET 请求
+    get root_path
     assert_response :success  # 确认得到表示成功的 HTTP 响应码 200 OK
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get static_pages_about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end

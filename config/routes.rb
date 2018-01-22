7f27ceb32a02a21e49fd0da3748639664ee3c4e4
janice <janicejiang.d@gmail.com>
2018-01-22 22:13:10 +0800

    User Signup: A First Step

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
+  get '/signup', to: 'users#new'
   get '/help', to: 'static_pages#help'
   get 'static_pages/about' # 会自动创建一个辅助方法 static_pages_about_url
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

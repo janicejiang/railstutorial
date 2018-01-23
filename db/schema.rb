6fa4684bd074b18cb390805cf9d68e1f3e3e9eb8
janice <janicejiang.d@gmail.com>
2018-01-23 20:53:59 +0800

    A Hashed Password

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123113936) do
+ActiveRecord::Schema.define(version: 20180123125155) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

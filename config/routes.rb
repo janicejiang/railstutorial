5ee4821fd7afc551604ca2921208e475082467b4
janice <janicejiang.d@gmail.com>
2018-01-24 21:32:50 +0800

    A Test for Invalid Submission

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
 
   get '/signup', to: 'users#new'
+  post 'signup', to: 'users#create'
   get '/help', to: 'static_pages#help'
   get 'static_pages/about' # 会自动创建一个辅助方法 static_pages_about_url
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

99924d1572e75104ccabe5c8153188f5df05952f
janice <janicejiang.d@gmail.com>
2018-01-25 15:13:43 +0800

    Sessions Controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   resources :users
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   get '/signup', to: 'users#new'
   post 'signup', to: 'users#create'
   get '/help', to: 'static_pages#help'

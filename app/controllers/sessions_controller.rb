4bb9aac29d0e39442cd1d55b97aeac201675aa6a
janice <janicejiang.d@gmail.com>
2018-01-25 15:57:56 +0800

    Flash Message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,11 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # 登入用户, 然后重定向到用户的资料页面
     else
-      # 创建一个错误消息
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
-  
+
   def destroy
   end
 end

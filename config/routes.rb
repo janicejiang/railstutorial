eea41209f1dbc19e605fc8f5e5479bac067d0afb
janice <janicejiang.d@gmail.com>
2018-02-07 23:51:41 +0800

    Account Activations Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   resources :users
+  resources :account_activations, only: [:edit]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
   get '/signup', to: 'users#new'
   post 'signup', to: 'users#create'
   get '/help', to: 'static_pages#help'
